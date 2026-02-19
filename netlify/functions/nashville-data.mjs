4babe90ac0ef8c0fb23f7ac0b773509061f138c3
CREDev124 <siedowm@gmail.com>
2026-02-18 21:11:35 -0600

    Update nashville-data.mjs

--- netlify/functions/nashville-data.mjs
+++ netlify/functions/nashville-data.mjs
@@ -1,36 +1,25 @@
 export default async (request, context) => {
   try {
-    var urls = [
-      "https://maps.nashville.gov/arcgis/rest/services/Planning/DevTracker_Cases/FeatureServer/1/query?where=1%3D1&outFields=*&f=json&returnGeometry=true",
-      "https://maps.nashville.gov/arcgis/rest/services/Planning/DevTracker_Cases/FeatureServer/0/query?where=1%3D1&outFields=*&f=json&returnGeometry=true",
-      "https://maps.nashville.gov/arcgis/rest/services/Planning/DevTracker_Cases/FeatureServer/1/query?where=OBJECTID%3E0&outFields=*&f=json&returnGeometry=true",
-      "https://maps.nashville.gov/arcgis/rest/services/Planning/DevTracker_Cases/FeatureServer/0/query?where=OBJECTID%3E0&outFields=*&f=json&returnGeometry=true"
-    ];
+    var params = new URLSearchParams({
+      where: "1=1",
+      outFields: "*",
+      outSR: "4326",
+      f: "json",
+      resultRecordCount: "4000",
+      returnGeometry: "true"
+    });
 
-    for (var i = 0; i < urls.length; i++) {
-      var res = await fetch(urls[i]);
-      var text = await res.text();
-      try {
-        var json = JSON.parse(text);
-        if (json.features && json.features.length > 0) {
-          return new Response(text, {
-            status: 200,
-            headers: {
-              "Content-Type": "application/json",
-              "Access-Control-Allow-Origin": "*",
-              "Cache-Control": "public, s-maxage=3600, max-age=3600"
-            }
-          });
-        }
-      } catch (e) {}
-    }
+    var url = "https://services2.arcgis.com/HdTo6HJqh92wn4D8/arcgis/rest/services/Development_Tracker_Cases_view/FeatureServer/1/query?" + params.toString();
+    var res = await fetch(url);
+    var text = await res.text();
 
-    return new Response(JSON.stringify({
-      error: "All queries returned 0 features",
-      lastResponse: text ? text.substring(0, 500) : "empty"
-    }), {
-      status: 502,
-      headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" }
+    return new Response(text, {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Cache-Control": "public, s-maxage=3600, max-age=3600"
+      }
     });
   } catch (err) {
     return new Response(
